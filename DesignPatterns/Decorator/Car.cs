7d55adda5c3e55a089251f1a97af84c10b75b767
Tomasz Krzyzak <tokrzyzak@gmail.com>
2021-06-09 19:33:09 +0200

    Decorator implementation

---
+++ DesignPatterns/Decorator/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Decorator
+{
+    class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("driving");
+        }
+    }
+}
---
+++ DesignPatterns/Decorator/CarDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Decorator
+{
+    class CarDecorator : ICar
+    {
+        ICar car;
+        public CarDecorator(ICar car)
+        {
+            this.car = car;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Start");
+            car.Drive();
+            Console.WriteLine("Stop");
+        }
+    }
+}
