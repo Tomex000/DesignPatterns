0aa8b5d62b41a6d732a3e9bfadd2ae46bdb6f9c6
Tomasz Krzyzak <tokrzyzak@gmail.com>
2021-06-09 19:57:28 +0200

    Builder implementation

---
+++ DesignPatterns/Builder/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder
+{
+    enum Color
+    {
+        Red,
+        Black,
+    }
+    class Car
+    {
+        private Color color;
+        private int doorsNumber;
+        private bool hasRoof;
+
+        public Car(Color color, int doorsNumber, bool hasRoof)
+        {
+            this.color = color;
+            this.doorsNumber = doorsNumber;
+            this.hasRoof = hasRoof;
+        }
+    }
+}
