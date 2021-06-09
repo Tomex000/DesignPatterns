ad594b69e67726f2ee390ac5d80e0d8ef7baaf96
Tomasz Krzyzak <tokrzyzak@gmail.com>
2021-06-09 20:06:50 +0200

    Facory implementation

---
+++ DesignPatterns/Factory/CarFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Factory
+{
+    class CarFactory
+    {
+        public Car GetSedan()
+        {
+            return new Sedan();
+        }
+
+        public Car GetSuv()
+        {
+            return new Suv();
+        }
+    }
+}
